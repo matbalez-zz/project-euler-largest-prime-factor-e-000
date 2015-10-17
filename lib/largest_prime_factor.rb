43b58a9c600a080ecfad060bf70dbb9aace05760
matbalez <matbalez@gmail.com>
2015-10-17 17:41:28 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,14 @@
-# Enter your procedural solution here!
+
+def largest_prime_factor(input)
+  i = 2
+  candidate = input
+  while candidate > 1 && i < candidate do
+    if candidate % i == 0
+      candidate = candidate / i
+      i = 2
+    else
+      i += 1
+    end
+  end
+  candidate
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+
+class LargestPrimeFactor
+  def initialize(num)
+    @num = num
+  end
+
+  def number()
+    i = 2
+    candidate = @num
+    while candidate > 1 && i < candidate do
+      if candidate % i == 0
+        candidate = candidate / i
+        i = 2
+      else
+        i += 1
+      end
+    end
+    candidate
+  end
+
+end
+
